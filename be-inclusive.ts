21736a66bfeb9a0dcec427955989d0d1722f68af
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-11 11:35:46 -0400

    ...

---
+++ be-inclusive.ts
@@ -0,0 +1,6 @@
+import {XtalDecor, XtalDecorCore} from 'xtal-decor/xtal-decor.js';
+import { XtalDecorProps } from 'xtal-decor/types';
+import {CE} from 'trans-render/lib/CE.js';
+import { IObserve } from 'be-observant/types';
+import { insertAdjacentTemplate } from 'trans-render/lib/insertAdjacentTemplate.js';
+import {getElementToObserve, addListener} from 'be-observant/be-observant.js';

42c0d212c17a49bc788478d2a5a613835beb2bab
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-13 10:17:54 -0500

    0.0.12

--- trPlugin.js
+++ trPlugin.js
@@ -1,30 +1,13 @@
 import { register } from 'trans-render/lib/pluginMgr.js';
-import { Includer } from './Includer.js';
 export const trPlugin = {
     selector: 'beInclusiveAttribs',
     ready: true,
-    processor: async ({ target, val, attrib, host, shadowPeer, ctx }) => {
-        delete ctx?.queryCache;
-        let vp;
-        if (val?.startsWith('{')) {
-            vp = JSON.parse(val);
-            vp.ctx = ctx;
-        }
-        else if (val) {
-            vp = {
-                of: val,
-                ctx: ctx,
-                transform: ctx.transform,
-                model: ctx.host,
-            };
-        }
-        if (vp !== undefined) {
-            const includer = new Includer(target, target, vp, shadowPeer);
-            await includer.onOf(includer);
-            target.removeAttribute('be-inclusive');
-            //includer.didInclude = true;
-            //passTheBaton('inclusive', target!, includer);
-        }
+    processor: async ({ target, val, attrib, host }) => {
+        if (customElements.get('be-inclusive') === undefined)
+            return;
+        const { attach } = await import('be-decorated/upgrade.js');
+        const instance = document.createElement('be-inclusive');
+        attach(target, 'inclusive', instance.attach.bind(instance));
     }
 };
 register(trPlugin);
--- trPlugin.ts
+++ trPlugin.ts
@@ -1,35 +1,16 @@
 import {RenderContext, TransformPluginSettings} from 'trans-render/lib/types';
+import {DEMethods} from 'be-decorated/types';
 import {register} from 'trans-render/lib/pluginMgr.js';
-import {EndUserProps, VirtualProps} from './types';
-import {Includer} from './Includer.js';
-
 
 export const trPlugin: TransformPluginSettings = {
     selector: 'beInclusiveAttribs',
     ready: true,
-    processor: async({target, val, attrib, host, shadowPeer, ctx}: RenderContext) => {
-        delete ctx?.queryCache
-        let vp: VirtualProps | undefined;
-        if(val?.startsWith('{')){
-            vp = JSON.parse(val) as VirtualProps;
-            vp.ctx = ctx!; 
-        }else if(val){
-            vp = {
-                of: val,
-                ctx: ctx!,
-                transform: ctx!.transform,
-                model: ctx!.host,
-            };
-        }
-        if(vp !== undefined){
-            const includer = new Includer(target!, target!, vp, shadowPeer!);
-            await includer.onOf(includer);
-            target!.removeAttribute('be-inclusive');
-            //includer.didInclude = true;
-            //passTheBaton('inclusive', target!, includer);
-        }
-        
+    processor:  async ({target, val, attrib, host}: RenderContext) => {
+        if(customElements.get('be-inclusive') === undefined) return;
+        const {attach} = await import('be-decorated/upgrade.js');
+        const instance = document.createElement('be-inclusive') as any as DEMethods;
+        attach(target!, 'inclusive', instance.attach.bind(instance));
     }
-};
+}
 
 register(trPlugin);

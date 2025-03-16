feedcf715b249921ce02d9fb3c61b9f5211976a2
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-03-15 21:33:26 -0400

    Update README.md

--- legacy/be-inclusive.js
+++ legacy/be-inclusive.js
@@ -1,119 +1,63 @@
 import { BE, propDefaults, propInfo } from 'be-enhanced/BE.js';
 import { XE } from 'xtal-element/XE.js';
-import { register } from 'be-hive/register.js';
-import { upShadowSearch } from 'trans-render/lib/upShadowSearch.js';
-import { birtualize } from 'trans-render/lib/birtualize.js';
-const birtualized = new Set();
+import { Transform } from 'trans-render/Transform.js';
 export class BeInclusive extends BE {
     static get beConfig() {
         return {
             parse: true,
             primaryProp: 'of',
             primaryPropReq: true,
-            isParsedProp: 'isC',
+            isParsedProp: 'isParsed',
         };
     }
-    #didInclude = false;
-    #timeoutHandler = undefined;
-    onOf(self) {
-        if (this.#didInclude)
-            return;
-        const { enhancedElement } = self;
-        const { of, shadowRootMode, model, bePrepended } = self;
-        if (of === undefined)
-            return;
-        if (typeof of === 'string') {
-            this.doOneOf(self, enhancedElement, of, shadowRootMode, model, !!bePrepended);
-        }
-        else {
-            const { length } = of;
-            for (let i = 0; i < length; i++) {
-                const oneOf = of[i];
-                if (typeof oneOf === 'string') {
-                    this.doOneOf(self, enhancedElement, oneOf, shadowRootMode, model, !!bePrepended);
-                }
-                else {
-                    this.doOneOf(self, enhancedElement, oneOf.of, oneOf.shadowRootMode, model, !!bePrepended);
-                }
-            }
-        }
-        this.#didInclude = true;
-    }
-    #templateLookup = {};
-    #templSearcher(of, self, templContainer) {
-        let templ = this.#templateLookup[of];
-        const { enhancedElement } = self;
-        if (templ === undefined) {
-            if (templContainer instanceof HTMLTemplateElement) {
-                templ = templContainer.content.querySelector(`#${of}`);
-            }
-            if (!templ) {
-                templ = upShadowSearch(enhancedElement, of);
-            }
-            if (templ === null || !(templ instanceof HTMLTemplateElement)) {
-                console.error({ of, self, msg: "Could not locate template." });
-                return undefined;
+    async onInitModel(self) {
+        const { enhancedElement, initModel } = self;
+        const { content } = enhancedElement;
+        const slots = Array.from(content.querySelectorAll('[slot]'));
+        for (const slot of slots) {
+            const initValFrom = slot.getAttribute('init-val-from');
+            const slotName = slot.slot;
+            let val;
+            if (initValFrom === null) {
+                const { getSignalVal } = await import('be-linked/getSignalVal.js');
+                val = getSignalVal(slot);
+                console.log({ val });
             }
             else {
-                this.#templateLookup[of] = templ;
-            }
-        }
-        return templ;
-    }
-    templCloner(templ) {
-        let clone;
-        //TODO switch to blow-dry
-        // const beMemedId = templ.getAttribute('be-memed-id');
-        // if(beMemedId){
-        //     clone = getContent(beMemedId).cloneNode(true) as DocumentFragment;
-        // }else{
-        clone = templ.content.cloneNode(true);
-        //}
-        return clone;
-    }
-    doOneOf(self, target, of, shadowRootMode, model, prepend) {
-        const templ = this.#templSearcher(of, self);
-        if (templ === undefined)
-            return;
-        if (!birtualized.has(templ)) {
-            birtualized.add(templ);
-            birtualize(templ, this.#templateLookup, (of) => this.#templSearcher(of, self, templ), this.templCloner);
-        }
-        const clone = this.templCloner(templ);
-        //DTR.transform(clone, ctx);
-        const verb = prepend ? 'prepend' : 'append';
-        if (shadowRootMode !== undefined) {
-            if (target.shadowRoot === null) {
-                target.attachShadow({ mode: shadowRootMode });
-            }
-            target.shadowRoot[verb](clone);
-        }
-        else {
-            const slots = target.querySelectorAll('[slot]');
-            for (const slot of slots) {
-                const slotName = slot.getAttribute('slot');
-                const slotDestiny = clone.querySelector(`slot[name="${slotName}"]`);
-                if (slotDestiny !== null) {
-                    slotDestiny.appendChild(slot);
+                slot.removeAttribute('init-val-from');
+                if (initValFrom[0] === '.') {
+                    const { getVal } = await import('trans-render/lib/getVal.js');
+                    val = getVal({ host: slot }, initValFrom);
                 }
                 else {
-                    slot.remove();
+                    val = slot[initValFrom];
                 }
             }
-            target[verb](clone);
+            initModel[slotName] = val;
         }
+        return {
+            model: Object.assign({}, initModel),
+        };
     }
-    #lastModel;
-    async onModel(self) {
-        // const {enhancedElement, model, ctx} = self;
-        // if(model === this.#lastModel) return;
-        // ctx.host = model;
-        // await DTR.transform(enhancedElement.shadowRoot || enhancedElement, ctx);
+    async startWeaving(self) {
+        const { of, model, xform, enhancedElement, slotMap } = self;
+        enhancedElement.addEventListener('load', async (e) => {
+            const le = e;
+            const { clone } = le;
+            const { children } = clone;
+            for (const child of children) {
+                Transform(child, model, xform);
+            }
+            console.log({ le });
+        }, { once: true });
+        enhancedElement.setAttribute('slotmap', JSON.stringify(slotMap));
+        enhancedElement.setAttribute('href', of);
+        return {
+            resolved: true,
+        };
     }
 }
-const tagName = 'be-inclusive';
-const ifWantsToBe = 'inclusive';
-const upgrade = '*';
+export const tagName = 'be-inclusive';
 const xe = new XE({
     config: {
         tagName,
@@ -122,19 +66,18 @@ const xe = new XE({
         },
         propInfo: {
             ...propInfo,
-            bePrepended: {
-                type: 'Boolean'
-            }
         },
         actions: {
-            onOf: {
-                ifAllOf: ['of', 'isC']
+            // onOf:{
+            //     ifAllOf:['of', 'isC']
+            // },
+            onInitModel: {
+                ifAllOf: ['isParsed', 'initModel']
             },
-            onModel: {
-                ifAllOf: ['model',]
+            startWeaving: {
+                ifAllOf: ['isParsed', 'model', 'slotMap', 'of', 'xform'],
             }
         }
     },
     superclass: BeInclusive
 });
-register(ifWantsToBe, upgrade, tagName);
--- legacy/be-inclusive.ts
+++ legacy/be-inclusive.ts
@@ -1,131 +1,74 @@
 import {BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
 import {BEConfig} from 'be-enhanced/types';
 import {XE} from 'xtal-element/XE.js';
-import {Actions, AllProps, AP, PAP, ProPAP, POA} from './types';
+import {Actions, AllProps, AP, PAP, ProPAP, POA} from '../types';
 import {register} from 'be-hive/register.js';
-import {upShadowSearch} from 'trans-render/lib/upShadowSearch.js';
-import {birtualize} from 'trans-render/lib/birtualize.js';
+import {LoadEvent} from 'mount-observer/MountObserver.js';
+import {Transform} from 'trans-render/Transform.js';
 
-
-const birtualized = new Set<HTMLTemplateElement>();
-export class BeInclusive extends BE<AP, Actions> implements Actions{
+export class BeInclusive<TProps, TMethods, TElement = {}> extends BE<AP, Actions, HTMLTemplateElement> implements Actions{
     static override get beConfig(){
         return {
             parse: true,
             primaryProp: 'of',
             primaryPropReq: true,
-            isParsedProp: 'isC',
+            isParsedProp: 'isParsed',
         } as BEConfig
     }
 
-    #didInclude = false;
-
-
-    onOf(self: this){
-        if(this.#didInclude) return;
-        const {enhancedElement} = self;
-        const {of, shadowRootMode, model, bePrepended} = self;
-        if(of === undefined) return;
-        if(typeof of === 'string'){
-            this.doOneOf(self, enhancedElement, of, shadowRootMode, model, !!bePrepended);
-        }else{
-            const {length} = of;
-            for(let i = 0; i < length; i++){
-                const oneOf = of[i];
-                if(typeof oneOf === 'string'){
-                    this.doOneOf(self, enhancedElement, oneOf, shadowRootMode, model, !!bePrepended);
-                }else{
-                    this.doOneOf(self, enhancedElement, oneOf.of as string, oneOf.shadowRootMode, model, !!bePrepended);
-                }
-            }
-        }
-        this.#didInclude = true;
-    }
-
-    #templateLookup: {[key: string]: HTMLTemplateElement} = {};
-    #templSearcher(of: string, self: this, templContainer?: HTMLTemplateElement){
-        let templ = this.#templateLookup[of];
-        const {enhancedElement} = self;
-        if(templ === undefined){
-            if(templContainer instanceof HTMLTemplateElement){
-                templ = templContainer.content.querySelector(`#${of}`) as HTMLTemplateElement;
-            }
-            
-            if(!templ){
-                templ = upShadowSearch(enhancedElement, of) as HTMLTemplateElement;
-                
-            }
-
-            if(templ === null || !(templ instanceof HTMLTemplateElement)){
-                console.error({of, self, msg:"Could not locate template."});
-                return undefined;
+    async onInitModel(self: this){
+        const {enhancedElement, initModel} = self;
+        const {content} = enhancedElement;
+        const slots = Array.from(content.querySelectorAll('[slot]'));
+        
+        for(const slot of slots){
+            const initValFrom = slot.getAttribute('init-val-from');
+            const slotName = slot.slot;
+            let val: any;
+            if(initValFrom === null){
+                const {getSignalVal} = await import('be-linked/getSignalVal.js');
+                val = getSignalVal(slot);
+                console.log({val});
             }else{
-                this.#templateLookup[of] = templ;
-            }
-        }
-        return templ;       
-    }
-
-    templCloner(templ: HTMLTemplateElement){
-        let clone: DocumentFragment;
-
-        //TODO switch to blow-dry
-        // const beMemedId = templ.getAttribute('be-memed-id');
-        // if(beMemedId){
-        //     clone = getContent(beMemedId).cloneNode(true) as DocumentFragment;
-        // }else{
-            clone = templ.content.cloneNode(true) as DocumentFragment;
-        //}
-        return clone;
-    }
-    
-    doOneOf(self: this, target: Element, of: string, shadowRootMode: 'open' | 'closed' | undefined, model: any, prepend: boolean){
-        const templ = this.#templSearcher(of, self);
-        if(templ === undefined) return;
-        if(!birtualized.has(templ)){
-            birtualized.add(templ);
-            birtualize(templ, this.#templateLookup, (of: string) => this.#templSearcher(of, self, templ), this.templCloner);
-            
-        }
-        
-        const clone = this.templCloner(templ);
-        
-        //DTR.transform(clone, ctx);
-        const verb = prepend ? 'prepend' : 'append';
-        if(shadowRootMode !== undefined){
-            if(target.shadowRoot === null){
-                target.attachShadow({mode: shadowRootMode});
-            }
-            target.shadowRoot![verb](clone);
-        }else{
-            const slots =  target.querySelectorAll('[slot]');
-            for(const slot of slots){
-                const slotName = slot.getAttribute('slot')!;
-                const slotDestiny = clone.querySelector(`slot[name="${slotName}"]`);
-                if(slotDestiny !== null){
-                    slotDestiny.appendChild(slot);
+                slot.removeAttribute('init-val-from');
+                if(initValFrom[0] === '.'){
+                    const {getVal} = await import('trans-render/lib/getVal.js');
+                    val = getVal({host: slot}, initValFrom);
                 }else{
-                    slot.remove();
+                    val = (<any>slot)[initValFrom];
                 }
             }
-            target[verb](clone);
+            (<any>initModel)[slotName] = val;
+
+        }
+        return {
+            model: Object.assign({}, initModel),
         }
     }
 
-    #lastModel: any;
-    async onModel(self: this){
-        // const {enhancedElement, model, ctx} = self;
-        // if(model === this.#lastModel) return;
-        // ctx.host = model;
-        // await DTR.transform(enhancedElement.shadowRoot || enhancedElement, ctx);
+    async startWeaving(self: this){
+        const {of, model, xform, enhancedElement, slotMap} = self;
+        enhancedElement.addEventListener<'load'>('load', async e => {
+            const le = e as LoadEvent;
+            const {clone} = le;
+            const {children} = clone;
+            for(const child of children){
+                Transform<any, any, any>(child, model, xform!);
+            }
+            
+            console.log({le});
+        }, {once: true});
+        enhancedElement.setAttribute('slotmap', JSON.stringify(slotMap));
+        enhancedElement.setAttribute('href', of);
+        return {
+            resolved: true,
+        }
     }
 }
 
-export interface BeInclusive extends AllProps<any, any, any>{}
+export interface BeInclusive<TProps, TMethods, TElement = {}> extends AllProps<TProps, TMethods, TElement>{}
 
-const tagName = 'be-inclusive';
-const ifWantsToBe = 'inclusive';
-const upgrade = '*';
+export const tagName = 'be-inclusive';
 
 const xe = new XE<AP, Actions>({
     config:{
@@ -135,20 +78,19 @@ const xe = new XE<AP, Actions>({
         },
         propInfo:{
             ...propInfo,
-            bePrepended: {
-                type: 'Boolean'
-            }
+            
         },
         actions:{
-            onOf:{
-                ifAllOf:['of', 'isC']
+            // onOf:{
+            //     ifAllOf:['of', 'isC']
+            // },
+            onInitModel:{
+                ifAllOf: ['isParsed', 'initModel']
             },
-            onModel:{
-                ifAllOf:['model',]
+            startWeaving:{
+                ifAllOf: ['isParsed', 'model', 'slotMap', 'of', 'xform'],
             }
         }
     },
     superclass: BeInclusive
 });
-
-register(ifWantsToBe, upgrade, tagName);

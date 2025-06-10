a00fe4879cfc6bdabde0cae7c04f58627033d773
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-10 19:31:33 -0400

    Update README.md

--- be-inclusive.js
+++ be-inclusive.js
@@ -11,5 +11,109 @@ import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
  * @implements {Actions}
  */
 class BeInclusive extends BE {
-    
+        /**
+     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement>}
+     */
+    static config = {
+        propInfo:{
+            includeRules: {},
+            nodesToInclude: {},
+        },
+        compacts:{
+            when_includeRules_changes_call_hydrate: 0,
+        },
+        actions:{
+            include:{
+                ifAllOf: ['nodesToInclude', 'includeRules'],
+            }
+        }
+    }
+
+        /**
+     * @type {WeakSet<Element>}
+     */
+    #alreadyProcessed = new WeakSet();
+
+    /**
+     * @type {Array<MutationObserver>}
+     */
+    #mutationObservers = [];
+
+    /**
+     * @type {Array<IMountObserver>}
+     */
+    #kindredObservers = [];
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async hydrate(self){
+        const {enhancedElement, includeRules} = self;
+        const {find} = await import('trans-render/dss/find.js');
+        /**
+         * @type {Array<Element>}
+         */
+        let nodesToInclude = [];
+        for(const includeRule of includeRules){
+            const {remoteSpecifier} = includeRule;
+            const target =  /**  @type {Element} */  (await find(enhancedElement, remoteSpecifier));
+            if(target === undefined || !(target instanceof HTMLTemplateElement)) continue;
+            const {content} = target;
+            nodesToInclude = [...nodesToInclude, ...Array.from(content.children)];
+            const config = { attributes: true, childList: true, subtree: true };
+            const callback = (mutationList, observer) => {
+                for (const mutation of mutationList) {
+                    const {addedNodes} = mutation;
+                    const addedElements = Array.from(addedNodes).filter(x => x instanceof Element);
+                    const newNodesToInclude = [...self.nodesToInclude, ...addedElements];
+                    self.nodesToInclude = newNodesToInclude;
+                }
+                
+            };
+            // Create an observer instance linked to the callback function
+            const observer = new MutationObserver(callback);
+
+            // Start observing the target node for configured mutations
+            observer.observe(content, config);
+            this.#mutationObservers.push(observer);
+        }
+        
+        return /* @type {PAP} */ ({
+            nodesToInclude
+        });
+    }
+
+    /**
+     * 
+     * @param {Element} enhancedElement 
+     */
+    async detachedCallback(enhancedElement){
+        for(const mo of this.#mutationObservers){
+            mo.disconnect();
+        }
+
+        for(const mo of this.#kindredObservers){
+            mo.disconnect(enhancedElement);
+        }
+    }
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async include(self){
+        const { enhancedElement, nodesToInclude } = self;
+        const {beKindred}  = await import('mount-observer/slotkin/beKindred.js');
+        for(const node of nodesToInclude){
+            if(this.#alreadyProcessed.has(node)) continue;
+            this.#alreadyProcessed.add(node);
+            this.#kindredObservers.push(beKindred(enhancedElement, node));
+        }
+        return /* @type {PAP} */ {
+            nodesToInclude: []
+        }
+    }
 }
